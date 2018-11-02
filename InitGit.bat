637da46b4e9aa0b32dc143c05dd7efb6aceb63e2
Daniel-Pimenta <30393490+Daniel-Pimenta@users.noreply.github.com>
2018-11-02 00:24:17 -0300

    Segundo commit

--- InitGit.bat
+++ InitGit.bat
@@ -1,5 +1,7 @@
 cd C:\Dados\Dev\eclipse\workspace-sts\b2w-rest-api
+echo "# Desafio-B2W" >> README.md
 git init
 git add .
-git commit -m "Primeiro commit !!!"
-
+git commit -m "first commit"
+git remote add origin https://github.com/Daniel-Pimenta/Desafio-B2W.git
+git push -u origin master
--- MeuGit.bat
+++ MeuGit.bat
@@ -1,8 +1,8 @@
 cd C:\Dados\Dev\eclipse\workspace-sts\b2w-rest-api
 git init
-git remote add origin https://github.com/Daniel-Pimenta/b2w-rest-api
-git pull origin master
 git add .
 git commit -m "Primeiro commit !!!"
+
+git remote add origin https://github.com/Daniel-Pimenta/b2w-rest-api
 git push origin master
 
